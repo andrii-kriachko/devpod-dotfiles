54283a4d116cf06815e250f798c88571b293af50
Andrii Kriachko <andrii_kriachko_2001@proton.me>
2025-11-25 16:48:38 +0200

    added setup to staged files

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# installing nix package manager
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
+. /home/vscode/.nix-profile/etc/profile.d/nix.sh
+
+# enabling nix flakes
+mkdir "$HOME"/.config/nix
+touch "$HOME"/.config/nix/nix.conf
+echo "experimental-features = nix-command flakes" >> "$HOME"/.config/nix/nix.conf
+
+# installing packages
+nix_packages=(
+  "neovim"
+  )
+for package in "${nix_packages[@]}"; do
+  nix profile add nixpkgs#$package
+done
+
+# copiing dotfiles
+ln -sf "$PWD/nvim" "$HOME"/.config/nvim
